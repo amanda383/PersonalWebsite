1366c11507d41a88372375fd554c692cb945edc1
Amanda <a383li@uwaterloo.ca>
2022-04-29 16:47:33 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,10 +12,9 @@ function sendEmail(params){
         message:document.getElementById("message").value
     };
     emailjs.send('service_n1pdoni','template_591ndcn', tempParams ).then(function(res){
-        alert('success')
         console.log("success", res.status)
     })
-
+    alert('success')
 }
 
 $(document).ready(function(){
